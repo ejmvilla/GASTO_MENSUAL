ebfd34dd55c0c58a82d11bad5155a9fa8d7813ad
Eugenio Villa <ejmvilla@hotmail.com>
2024-09-05 19:53:17 -0300

    static 9

--- GASTOS/settings.py
+++ GASTOS/settings.py
@@ -146,11 +146,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / 'static'),
-    '/var/task/staticfiles',
+    '/var/task/static',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
