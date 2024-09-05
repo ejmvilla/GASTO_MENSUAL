7655a6fb70a6192ce9c2531bc8df0ddd2e9d6df4
Eugenio Villa <ejmvilla@hotmail.com>
2024-09-05 19:46:14 -0300

    static 8

--- GASTOS/settings.py
+++ GASTOS/settings.py
@@ -150,6 +150,7 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR / 'static'),
+    '/var/task/staticfiles',
 ]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
